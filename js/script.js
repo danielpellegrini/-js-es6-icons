1fe4d96006105f9390cc748e2b01c41335a3207e
Daniel <danielpellegrini87@gmail.com>
2021-01-21 16:47:24 +0100

    milestone 1 completed

--- js/script.js
+++ js/script.js
@@ -1,97 +1,110 @@
 // Milestone 1:
 // - Definire un array di oggetti; ogni oggetto rappresenta un'icona, che è caratterizzata da: nome, prefisso, tipo e famiglia.
-// - Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
 
+$(document).ready(function() {
 
-const icons = [
-  {
-    name: 'cat',
-    prefix: 'fa',
-    type: 'animal',
-    family: 'fas'
-  },
-  {
-    name: 'crow',
-    prefix: 'fa',
-    type: 'animal',
-    family: 'fas'
-  },
-  {
-    name: 'dog',
-    prefix: 'fa',
-    type: 'animal',
-    family: 'fas'
-  },
-  {
-    name: 'dove',
-    prefix: 'fa',
-    type: 'animal',
-    family: 'fas'
-  },
-  {
-    name: 'horse',
-    prefix: 'fa',
-    type: 'animal',
-    family: 'fas'
-  },
-  {
-    name: 'hippo',
-    prefix: 'fa',
-    type: 'animal',
-    family: 'fas'
-  },
-  {
-    name: 'fish',
-    prefix: 'fa',
-    type: 'animal',
-    family: 'fas'
-  },
-  {
-    name: 'carrot',
-    prefix: 'fa',
-    type: 'vegetable',
-    family: 'fas'
-  },
-  {
-    name: 'lemon',
-    prefix: 'fa',
-    type: 'vegetable',
-    family: 'fas'
-  },
-  {
-    name: 'pepper-hot',
-    prefix: 'fa',
-    type: 'vegetable',
-    family: 'fas'
-  },
-  {
-    name: 'user-astronaut',
-    prefix: 'fa',
-    type: 'user',
-    family: 'fas'
-  },
-  {
-    name: 'user-graduate',
-    prefix: 'fa',
-    type: 'animal',
-    family: 'fas'
-  },
-  {
-    name: 'user-secret',
-    prefix: 'fa',
-    type: 'user',
-    family: 'fas'
-  },
-]
+  const icons = [{
+      name: 'cat',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas'
+    },
+    {
+      name: 'crow',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas'
+    },
+    {
+      name: 'dog',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas'
+    },
+    {
+      name: 'dove',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas'
+    },
+    {
+      name: 'horse',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas'
+    },
+    {
+      name: 'hippo',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas'
+    },
+    {
+      name: 'fish',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas'
+    },
+    {
+      name: 'carrot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas'
+    },
+    {
+      name: 'lemon',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas'
+    },
+    {
+      name: 'pepper-hot',
+      prefix: 'fa-',
+      type: 'vegetable',
+      family: 'fas'
+    },
+    {
+      name: 'user-astronaut',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas'
+    },
+    {
+      name: 'user-graduate',
+      prefix: 'fa-',
+      type: 'animal',
+      family: 'fas'
+    },
+    {
+      name: 'user-secret',
+      prefix: 'fa-',
+      type: 'user',
+      family: 'fas'
+    }
+  ];
 
-console.log(icons);
+  // - Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
 
-const iconsContainer = document.getElementById('main-container');
-
-
+  const iconsContainer = document.getElementById('main-container');
 
 
 
+  icons.forEach((item) => {
+
+    const {name, prefix, type, family} = item;
+
+
+    iconsContainer.innerHTML += `
+    <div>
+      <i class="${family} ${prefix}${name}"></i>
+      <div class="title">${name}</div>
+    </div>
+    `
+  });
+
+
+
+});
 
 // Milestone 2:
 // - Definire un array di colori e associare ad ogni tipo di icona un colore.
@@ -111,3 +124,19 @@ const iconsContainer = document.getElementById('main-container');
 // Leggetevi prima il tutto giusto per avere chiara l'idea sulla nostra applicazione, ma poi stop.
 // Prendete quindi solo la prima milestone e conquistatela senza più neanche pensare alle successive. E poi caffettino, e passate alla seconda.
 // Per la terza milestone un po' di codice l'abbiamo già visto in classe. Approcciate con curiosità e creatività il tutto.
+
+
+/*
+<i class="fas fa-crow"></i>
+<i class="fas fa-dog"></i>
+<i class="fas fa-dove"></i>
+<i class="fas fa-horse"></i>
+<i class="fas fa-hippo"></i>
+<i class="fas fa-fish"></i>
+<i class="fas fa-carrot"></i>
+<i class="fas fa-lemon"></i>
+<i class="fas fa-pepper-hot"></i>
+<i class="fas fa-user-astronaut"></i>
+<i class="fas fa-user-graduate"></i>
+<i class="fas fa-user-secret"></i>
+*/
